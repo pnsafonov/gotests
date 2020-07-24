32c85d6ddbeab80327be3df22cdc15ec62a4541c
Pavel Safonov <p.n.safonov@gmail.com>
2020-07-24 21:08:13 +0300

    server impl

--- services/srv2/server/logic/impl.go
+++ services/srv2/server/logic/impl.go
@@ -13,12 +13,20 @@ type ServerImpl struct {
 func (srv *ServerImpl) Login(ctx context.Context, userName string, passHash string) (*srv2.UserInfo, bool, error) {
     log.Printf("Login username = %s, passHash = %s\n", userName, passHash)
 
+    //if len(userName) != 0 {
+    //    return nil, false, errors.New("fail in Login")
+    //}
+
     ui := &srv2.UserInfo{
        Id:       7,
        FistName: "SomeFirstName1",
        LastName: "SomeLastName",
        Email:    "some_email@gmail.com",
     }
+
+    //if len(userName) != 0 {
+    //    return ui, true, errors.New("fail in Login")
+    //}
 
     return ui, true, nil
 }
