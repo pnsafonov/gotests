985e31e734774b08097f727cc5eaa6b1eca657a0
Pavel Safonov <p.n.safonov@gmail.com>
2020-07-26 22:44:17 +0300

    test

--- tests/pgx/pgx_test.go
+++ tests/pgx/pgx_test.go
@@ -5,6 +5,7 @@ import (
     "fmt"
     "github.com/jackc/pgx/v4"
     "github.com/jackc/pgx/v4/pgxpool"
+    "log"
     "os"
     "testing"
     "time"
@@ -52,8 +53,7 @@ func TestPgx1(t *testing.T) {
 
 }
 
-func TestPgx2(t *testing.T) {
-    connString :=  "host=172.77.10.15 port=5432 user=test password=6397c7f7f97a dbname=dvdrental pool_min_conns=1 pool_max_conns=10"
+func getConnPool0(connString string) *pgxpool.Pool {
     var (
         poolCfg *pgxpool.Config
         pool    *pgxpool.Pool
@@ -80,13 +80,23 @@ func TestPgx2(t *testing.T) {
         fmt.Fprintf(os.Stderr, "err: %v\n", err)
         os.Exit(1)
     }
+    return pool
+}
 
+func getConnPool1() *pgxpool.Pool {
+    connString := "host=172.77.10.15 port=5432 user=test password=6397c7f7f97a dbname=dvdrental pool_min_conns=1 pool_max_conns=10"
+    return getConnPool0(connString)
+}
+
+func TestPgx2(t *testing.T) {
+    pool := getConnPool1()
+
+    ctx := context.Background()
     count := 100
-
     for i := 0; i < count; i++ {
         var title string
         var releaseYear int
-        err = pool.QueryRow(context.Background(), "select title, release_year from film where film_id=$1", 42).Scan(&title, &releaseYear)
+        err := pool.QueryRow(ctx, "select title, release_year from film where film_id=$1", 42).Scan(&title, &releaseYear)
         if err != nil {
             fmt.Fprintf(os.Stderr, "QueryRow failed: %v\n", err)
             //os.Exit(1)
@@ -96,6 +106,61 @@ func TestPgx2(t *testing.T) {
 
         fmt.Printf("title = %s, year = %d\n", title, releaseYear)
     }
+}
+
+type film3 struct {
+    title       string
+    year        int
+    category    string
+    lang        string
+}
+
+func (f *film3) load(rows pgx.Rows) (bool, error) {
+    if !rows.Next() {
+        return false, nil
+    }
+    err := rows.Scan(&f.title, &f.year, &f.category, &f.lang)
+    if err != nil {
+        return false, err
+    }
+    return true, nil
+}
 
 
+func TestPgx3(t *testing.T) {
+    pool := getConnPool1()
+
+    q :=
+`
+SELECT t0.title, t0.release_year, t2.name, t3.name
+FROM film as t0
+JOIN film_category as t1
+	ON t0.film_id = t1.film_id
+JOIN category as t2
+	ON t1.category_id = t2.category_id
+JOIN language as t3
+	ON t0.language_id = t3.language_id
+ORDER BY t0.film_id desc
+LIMIT 10
+`
+    ctx := context.Background()
+    rows, err := pool.Query(ctx, q)
+    if err != nil {
+        fmt.Fprintf(os.Stderr, "QueryRow failed: %v\n", err)
+        os.Exit(1)
+    }
+
+    films := make([]film3, 0, 16)
+
+    for rows.Next() {
+        f := film3{}
+        err := rows.Scan(&f.title, &f.year, &f.category, &f.lang)
+        if err != nil {
+            fmt.Fprintf(os.Stderr, "scan failed: %v\n", err)
+            os.Exit(1)
+        }
+        films = append(films, f)
+    }
+
+    log.Println("done")
 }
