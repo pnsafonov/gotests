29623aa7474aa89fcba751fec68b5a671a0bc320
p.safonov <p.safonov@zonatelecom.ru>
2020-08-12 16:35:42 +0300

    test

--- tests/all/all_test.go
+++ tests/all/all_test.go
@@ -37,3 +37,22 @@ func TestCeil(t *testing.T) {
 
 	fmt.Println("done")
 }
+
+//func TestSumNullable1(t *testing.T) {
+//	a1 := sql.NullInt64{ Valid: false, Int64: 1}
+//	a2 := sql.NullInt64{ Valid: false, Int64: 2}
+//	r1 := a1 + a2
+//}
+
+type keyIntStr struct {
+	id 	 int
+	name string
+}
+
+func TestMap1(t *testing.T) {
+	m1 := make(map[keyIntStr]int)
+	m1[keyIntStr{id: 1, name: "11"}] = 1
+	m1[keyIntStr{id: 1, name: "11"}] = 2
+	m1[keyIntStr{id: 1, name: "11"}] = 3
+	fmt.Println("done")
+}
